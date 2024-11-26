8823d9a3898fdf71fa6f225ecf13cb570818d13c
Marc Mas <marcmashernandez@gmail.com>
2024-11-26 11:34:38 +0100

    Actualizats tots

--- CODE INSTITUT/ServeisXarxa/PRACTIQUES/Scripts/SumaDosV2.sh
+++ CODE INSTITUT/ServeisXarxa/PRACTIQUES/Scripts/SumaDosV2.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo BON DIA. ARA SUMARE DOS NUMEROS
-echo $(($1+$2))
+echo Numero 1?
+read n1
+echo Numero 2?
+read n2
+a=$((n1+n2))
+b=$((n1-n2))
+c=$((n1*n2))
+echo La suma es $a, la resta es $b, la multiplicacio es $c
