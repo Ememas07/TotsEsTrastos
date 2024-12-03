68f806fced32c5a3ee285643c21909946b8b06d5
Marc Mas <marcmashernandez@gmail.com>
2024-12-03 09:03:49 +0100

    Comencar arxius de fitxers

---
+++ CODE INSTITUT/NATXO/JAVA/ExercisisDiscoDur/src/ExercisiFitxers01.java
@@ -0,0 +1,22 @@
+import java.io.FileReader;
+
+
+public class ExercisiFitxers01 {
+    // ) Crea un fichero de texto con el nombre y contenido que tu quieras. Ahora
+    // crea una aplicación que lea este fichero de texto carácter a carácter y
+    // muestre su contenido por pantalla sin espacios. Por ejemplo, si un fichero
+    // tiene el siguiente texto «Esto es una prueba», deberá mostrar
+    // «Estoesunaprueba».
+    public static void main(String[] args) {
+        final String nomFichero="../../JAVA/FitxersDeText/hola.txt";
+        try(FileReader fr=new FileReader (nomFichero)){
+            int valor=fr.read();
+            while(valor!=-1){
+                if(valor!=32){
+                    System.out.print((char)valor);
+                }
+                valor=fr.read();
+            }
+        }
+    }
+}
