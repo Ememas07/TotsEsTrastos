70e7a76ce3cf5d7be2106870c5291d6c907d1da9
Ememas07 <marcmashernandez@gmail.com>
2024-12-04 21:52:13 +0100

    Acabam Joc

--- CODE INSTITUT/ServeisXarxa/PRACTIQUES/Scripts/Pràctica 1/Completament Automatic/autojocEFICIENT.sh
+++ CODE INSTITUT/ServeisXarxa/PRACTIQUES/Scripts/Pràctica 1/Completament Automatic/autojocEFICIENT.sh
@@ -8,6 +8,9 @@ startup(){
     lower=0
     iteracions=0
     intents="" # Variable per guardar tots els intents
+    total_time=0
+    total_iteracions=0
+    total_execucions=0
     title_screen
     for ((i=1; i<=executions; i++)); do
         echo "Execució $i..."
@@ -18,19 +21,27 @@ startup(){
         intents=""
         inici
     done
+    sort -t'-' -k2n TaulaIntentosAUTO.txt | while IFS=" - " read -r intents iteracions_descripcio
+    do
+        # Separar la descripció de les iteracions i el número final
+        iteracions=$(echo "$iteracions_descripcio" | cut -d' ' -f1)
+        descripcio=$(echo "$iteracions_descripcio" | sed 's/^[^a-zA-Z0-9]*//')
+        
+        # Mostrar amb format d'amplada fixa
+        printf "%-50s %-15s %-30s %-15s\n" "$intents" "$iteracions iteracions" "$descripcio" "$execution_time ms"
+    done
     avg_time=$((total_time / total_execucions))  # Mitjana de temps
     avg_iteracions=$((total_iteracions / total_execucions)) # Mitjana de iteracions
-    echo "#######################################################"
-    echo "Mitjana de temps de execució: $avg_time ms"
-    echo "Mitjana de iteracions: $avg_iteracions"
-    echo "#######################################################"
+    echo la mitja del temps es $avg_time ms
+    echo la mitja de iteracions es $avg_iteracions
 }
 
 inici(){
     nom=IA
     nombre=$(($((RANDOM))%$mesalt))
-    start_time=$(date +%s%3N)  # Temps d'inici en milisegons    
-    check    
+    start_time=$(date +%s%3N)  # Temps d'inici en milisegons
+    check
+    
 }
 check(){
     intento=$(((lower + upper) / 2))
@@ -67,35 +78,15 @@ sortir(){
 }
 finalitzar(){
     end_time=$(date +%s%3N)  # Temps final en milisegons
-    execution_time=$((end_time - start_time))   
+    execution_time=$((end_time - start_time)) 
     clear
     echo "Enhorabona $nom, has guanyat en $iteracions intents!"
 
     # Afegir la línia al fitxer amb format fix, incloent el temps d'execució
     printf "%s - %d iteracions a %d, entre 0 i %d, Temps: %d ms\n" "$intents" "$iteracions" "$nombre" "$mesalt" "$execution_time" >> TaulaIntentosAUTO.txt
-
-    # Ordenar el fitxer per iteracions i mostrar en temps real
-    echo "#######################################################"
-    echo "#   Resultats ordenats per iteracions:               #"
-    echo "-------------------------------------------------------"
-
-    # Primer, imprimeix els títols de les columnes
-    printf "%-50s %-15s %-30s %-15s\n" "Intents" "Iteracions" "Descripció" "Temps"
-
     total_time=$((total_time + execution_time))  # Sumar el temps d'execució
     total_iteracions=$((total_iteracions + iteracions))  # Sumar les iteracions
     total_execucions=$((total_execucions + 1))  # Incrementar el nombre d'execucions
-    # Ordena i mostra amb awk: ordenar per iteracions (a la segona columna)
-    sort -t'-' -k2n TaulaIntentosAUTO.txt | while IFS=" - " read -r intents iteracions_descripcio
-    do
-        # Separar la descripció de les iteracions i el número final
-        iteracions=$(echo "$iteracions_descripcio" | cut -d' ' -f1)
-        descripcio=$(echo "$iteracions_descripcio" | sed 's/^[^a-zA-Z0-9]*//')
-        
-        # Mostrar amb format d'amplada fixa
-        printf "%-50s %-15s %-30s %-15s\n" "$intents" "$iteracions iteracions" "$descripcio" "$execution_time ms"
-    done
-
     echo "#######################################################"
 }
 
