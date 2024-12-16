e8b6ddc0b448560dad635d82679e31e7fdc7643c
Ememas07 <marcmashernandez@gmail.com>
2024-12-16 17:43:03 +0100

    thingy

---
+++ CODE INSTITUT/PERSONAL/ipv4getter.sh
@@ -0,0 +1,5 @@
+date > date.txt
+echo "#########################################" >> fitxer.txt
+cat date.txt >> fitxer.txt
+curl ifcfg.me >> fitxer.txt
+echo "#########################################" >> fitxer.txt
