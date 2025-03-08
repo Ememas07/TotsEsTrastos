5f47ab9b6c54f2702ffd8df2ca4fe2626e6f4c2d
Marc <marcmashernandez@gmail.com>
2025-03-08 07:54:06 +0100

    asd

---
+++ Picture Prepping BATs/MoveReplays.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\Marc Mas\Downloads"
+xcopy *.rpy 'C:\users\Marc Mas\Documents\Assetto Corsa Competizione\Replay\Saved\'
+del *.rpy
+xcopy *.Vcr 'C:\Program Files (x86)\Steam\steamapps\common\Le Mans Ultimate\UserData\Replays'
+del *.Vcr
