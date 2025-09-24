bf1f039f4162772de447c7b416988d070a2476d9
Marc Mas <marcmashernandez@gmail.com>
2025-09-24 17:58:55 +0200

    Netbeans i Netbeans a VSCODE

---
+++ CODE INSITUT CFGS/PROGRAMACIO/Java/ElMeuPrimerProjecte/src/main/java/com/mycompany/elmeuprimerprojecte/ElMeuPrimerProjecte.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.elmeuprimerprojecte;
+import java.util.Scanner;
+/**
+ *
+ * @author Marc Mas
+ */
+public class ElMeuPrimerProjecte {
+
+    public static void main(String[] args) {
+        System.out.println("Quants d'anys tens?");
+        Scanner s = new Scanner(System.in);
+        Integer Edat = s.nextInt();
+        System.out.println("l'any que ve tendras"+Edat+"Anys");
+    }
+}
