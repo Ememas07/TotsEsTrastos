1b25f876c424c243086ae0c73b48ee58a1602cad
Marc Mas <marcmashernandez@gmail.com>
2025-09-25 16:24:42 +0200

    Act13 i Act14 correctament formatades

--- CODE INSITUT CFGS/PROGRAMACIO/Java/Ud1Act14MarcMas/src/main/java/org/yourcompany/yourproject/Ud1Act14MarcMas.java
+++ CODE INSITUT CFGS/PROGRAMACIO/Java/Ud1Act14MarcMas/src/main/java/org/yourcompany/yourproject/Ud1Act14MarcMas.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.yourcompany.yourproject;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,24 @@ package org.yourcompany.yourproject;
 public class Ud1Act14MarcMas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Activitat 14
+        /*Solicita a l’usuari tres distàncies:
+        ○ La primera, en milímetres
+        ○ La segona, en centímetres
+        ○ La tercera, en metres
+        ● Dissenya un programa que mostri la suma de les tres longituds en centímetres*/
+        System.out.println("Indiqui la primera distancia, en milimetres");
+        Scanner s = new Scanner(System.in);
+        Integer distancia1 = s.nextInt();
+        System.out.println("Indiqui la segona distancia, en centimetres");
+        Scanner s2 = new Scanner(System.in);
+        Integer distancia2 = s2.nextInt();
+        System.out.println("Indiqui la tercera distancia, en metres");
+        Scanner s3 = new Scanner(System.in);
+        Integer distancia3 = s3.nextInt();
+        Double sumaDistancies = ((double) distancia1 / 10) + distancia2 + ((double) distancia3 * 100);
+        System.out.println("La suma de les distancies en centimetres es "+sumaDistancies);
+        
+
     }
 }
