15d46eeac818e877b2f416d8569b5baf5fac8fc9
Marc Mas <marcmashernandez@gmail.com>
2025-09-25 16:41:18 +0200

    Activitat 15

--- CODE INSITUT CFGS/PROGRAMACIO/Java/Ud1Act15MarcMas/src/main/java/org/yourcompany/yourproject/Ud1Act15MarcMas.java
+++ CODE INSITUT CFGS/PROGRAMACIO/Java/Ud1Act15MarcMas/src/main/java/org/yourcompany/yourproject/Ud1Act15MarcMas.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.yourcompany.yourproject;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,17 @@ package org.yourcompany.yourproject;
 public class Ud1Act15MarcMas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Activitat 15
+        System.out.println("Indiqui el nombre de formigues");
+        Scanner s = new Scanner(System.in);
+        Integer nFormigues = s.nextInt();
+        System.out.println("Indiqui el nombre de aranyes");
+        Scanner s2 = new Scanner(System.in);
+        Integer nAranyes = s2.nextInt();
+        System.out.println("Indiqui el nombre de centpeus");
+        Scanner s3 = new Scanner(System.in);
+        Integer nCentpeus = s3.nextInt();
+        Double sumaPotes = (double) nFormigues * 6 + (double) nAranyes * 8 + (double) nCentpeus * 100;
+        System.out.println("La suma de les potes es "+sumaPotes);
     }
 }
