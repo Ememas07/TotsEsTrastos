f4acedc0349909cfbb29eaf3fe1eeff90e46d609
Marc Mas <marcmashernandez@gmail.com>
2025-09-26 20:39:03 +0200

    Entorns

---
+++ CODE INSITUT CFGS/Entorns de Desenvolupament/exercisi-depuracio/bug.js
@@ -0,0 +1,7 @@
+function sumarNumeros(a,b){
+    const resultat = a + b;
+    return resultat;
+}
+
+ const suma = sumarNumeros(5,3);
+ console.log(`La suma és: ${suma}`);
---
+++ CODE INSITUT CFGS/Entorns de Desenvolupament/primera-aplicacio/index.js
@@ -0,0 +1,4 @@
+const _ = require('lodash');
+const array = [1,2,3,4,5];
+console.log("Array original:",array);
+console.log("Primer element de l'array:",_.first(array));
