ccd442720ce161a923df4e2bb9a6c72857b9fd54
Marc Mas <marcmashernandez@gmail.com>
2025-09-26 18:45:00 +0200

    Començada act5

--- CODE INSITUT CFGS/PROGRAMACIO/Java/UD2Act5MarcMas/src/main/java/org/yourcompany/yourproject/UD2Act5MarcMas.java
+++ CODE INSITUT CFGS/PROGRAMACIO/Java/UD2Act5MarcMas/src/main/java/org/yourcompany/yourproject/UD2Act5MarcMas.java
@@ -4,6 +4,8 @@
 
 package org.yourcompany.yourproject;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Marc Mas
@@ -11,6 +13,8 @@ package org.yourcompany.yourproject;
 public class UD2Act5MarcMas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Nombre A");
+        
     }
 }
