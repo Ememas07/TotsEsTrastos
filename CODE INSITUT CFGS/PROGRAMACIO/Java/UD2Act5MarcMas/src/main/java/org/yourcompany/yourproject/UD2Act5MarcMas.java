da26a99a31c565cb7d1dc3e89cd5f90b93d26c08
Marc Mas <marcmashernandez@gmail.com>
2025-09-29 17:08:30 +0200

    Activitat 6 Marc Mas

--- CODE INSITUT CFGS/PROGRAMACIO/Java/UD2Act5MarcMas/src/main/java/org/yourcompany/yourproject/UD2Act5MarcMas.java
+++ CODE INSITUT CFGS/PROGRAMACIO/Java/UD2Act5MarcMas/src/main/java/org/yourcompany/yourproject/UD2Act5MarcMas.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.yourcompany.yourproject;
 
 import java.util.Scanner;
@@ -15,6 +14,14 @@ public class UD2Act5MarcMas {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Nombre A");
-        
+        Double numA = s.nextDouble();
+        System.out.println("Nombre B");
+        Double numB = s.nextDouble();
+        System.out.println("Nombre C");
+        Double numC = s.nextDouble();
+        Double resultat1 = ((-numB + Math.sqrt(Math.pow(numB, 2) - (4 * numA * numC))) / (2 * numA));
+        Double resultat2 = ((-numB - Math.sqrt(Math.pow(numB, 2) - (4 * numA * numC))) / (2 * numA));
+        System.out.println("El resultat positiu es " + resultat1);
+        System.out.println("El resultat negatiu es " + resultat2);      
     }
 }
--- CODE INSITUT CFGS/PROGRAMACIO/Java/UD2Act6MarcMas/src/main/java/org/yourcompany/yourproject/UD2Act6MarcMas.java
+++ CODE INSITUT CFGS/PROGRAMACIO/Java/UD2Act6MarcMas/src/main/java/org/yourcompany/yourproject/UD2Act6MarcMas.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.yourcompany.yourproject;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,17 @@ package org.yourcompany.yourproject;
 public class UD2Act6MarcMas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Introdueix un numero entre -99.999 i 99.999");
+        Integer numero = s.nextInt();
+        if (numero > 99999 || numero < -99999) {
+            System.out.println("Nombre massa gran");
+        } else {
+            if (numero < 0) {
+                numero = -numero;
+            }
+            String llargaria = numero.toString();
+            System.out.println("El teu numero te " + llargaria.length() + " xifres");
+        }
     }
 }
