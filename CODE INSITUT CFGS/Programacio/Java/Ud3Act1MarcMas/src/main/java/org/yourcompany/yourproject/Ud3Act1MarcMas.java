a96b4b2d9d22eeb84fcf88adbd648df4077c6443
Marc Mas <marcmashernandez@gmail.com>
2025-10-01 17:49:26 +0200

    Començam UD3

---
+++ CODE INSITUT CFGS/Programacio/Java/Ud3Act1MarcMas/src/main/java/org/yourcompany/yourproject/Ud3Act1MarcMas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package org.yourcompany.yourproject;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Marc Mas
+ */
+public class Ud3Act1MarcMas {
+
+    public static void main(String[] args) {
+        System.out.println("Introdueixi un numero");
+        Scanner s = new Scanner(System.in);
+        Integer n = s.nextInt();
+        while (n != 0) {
+            if (true) {
+                
+            }
+            n = s.nextInt();
+
+        }
+    }
+}
