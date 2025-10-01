cb3a4530f367e4b4b4346bdd77d869d4121c894a
Marc Mas <marcmashernandez@gmail.com>
2025-10-01 09:50:18 +0200

    Arreglat un problema a Act1

--- CODE INSITUT CFGS/PROGRAMACIO/Java/UD2Act1MarcMas/src/main/java/org/yourcompany/yourproject/UD2Act1MarcMas.java
+++ CODE INSITUT CFGS/PROGRAMACIO/Java/UD2Act1MarcMas/src/main/java/org/yourcompany/yourproject/UD2Act1MarcMas.java
@@ -18,7 +18,7 @@ public class UD2Act1MarcMas {
         Scanner s = new Scanner(System.in);
         Integer n1 = s.nextInt();
         String parellImparell = "Parell";         
-        if (n1 % 2 == 1) {
+        if (Math.abs(n1) % 2 == 1) {
             parellImparell = "Imparell";          
         }
         String positiuNegatiu = "Negatiu";
