4fe192f934d8cbe72881a0893b0c3ddf89ececdd
Marc Mas <marcmashernandez@gmail.com>
2025-10-02 17:55:14 +0200

    Act11 Comentada i optimitzada

--- CODE INSITUT CFGS/Programacio/Java Acabat/UD1/Ud1Act11MarcMas/Activitat11/src/main/java/org/yourcompany/yourproject/Activitat11.java
+++ CODE INSITUT CFGS/Programacio/Java Acabat/UD1/Ud1Act11MarcMas/Activitat11/src/main/java/org/yourcompany/yourproject/Activitat11.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.yourcompany.yourproject;
 
 import java.util.Scanner;
@@ -15,17 +14,17 @@ public class Activitat11 {
     public static void main(String[] args) {
         // Activitat 11
 
-        System.out.println("Nota 1");
         Scanner s = new Scanner(System.in);
-        Integer nota1 = s.nextInt();
-        System.out.println("Nota 2");
-        Integer nota2 = s.nextInt();
-        System.out.println("Nota 3");
-        Integer nota3 = s.nextInt();
-        Double mitjaDecimals = (nota1.doubleValue() + nota2.doubleValue() + nota3.doubleValue()) / 3;
-        Integer mitjaEnters = (nota1 + nota2 + nota3) / 3;
-        System.out.println("La teva mitja amb decimals es "+mitjaDecimals);
-        System.out.println("La teva mitja sense decimals es "+mitjaEnters);
-        
+        System.out.println("Nota 1"); //deman la primera nota per consola
+        Byte nota1 = s.nextByte();
+        System.out.println("Nota 2"); //deman la segona nota per consola
+        Byte nota2 = s.nextByte();
+        System.out.println("Nota 3"); //deman la tercera nota per consola
+        Byte nota3 = s.nextByte();
+        Float mitjaDecimals = ((float) nota1 + (float) nota2 + (float) nota3) / 3; //calcul la mitjana amb decimals
+        Byte mitjaEnters = mitjaDecimals.byteValue(); // de la mitjana amb decimals trec la mitjana sense decimals
+        System.out.println("La teva mitja amb decimals es " + mitjaDecimals);
+        System.out.println("La teva mitja sense decimals es " + mitjaEnters);
+
     }
 }
