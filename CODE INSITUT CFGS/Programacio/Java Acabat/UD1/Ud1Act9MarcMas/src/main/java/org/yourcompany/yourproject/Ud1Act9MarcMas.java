ab229ec34d2ac0a2925149874808ee4c652726c6
Marc Mas <marcmashernandez@gmail.com>
2025-10-02 18:12:10 +0200

    Act9 canviada

--- CODE INSITUT CFGS/Programacio/Java Acabat/UD1/Ud1Act9MarcMas/src/main/java/org/yourcompany/yourproject/Ud1Act9MarcMas.java
+++ CODE INSITUT CFGS/Programacio/Java Acabat/UD1/Ud1Act9MarcMas/src/main/java/org/yourcompany/yourproject/Ud1Act9MarcMas.java
@@ -19,14 +19,14 @@ public class Ud1Act9MarcMas {
         System.out.println("Plou? (respon amb 'true' or 'false')");
         Boolean plou = s.nextBoolean(); 
         System.out.println("Has acabat les feines? (respon amb 'true' or 'false')");
-        Boolean feines = s.nextBoolean();
+        Boolean feinesAcabades = s.nextBoolean();
         System.out.println("Has d'anar a classe? (respon amb 'true' or 'false')");
         Boolean classe = s.nextBoolean();
-        boolean potsSortir = ((plou == false && feines == true) || classe == true);
+        boolean potsSortir = ((plou == false && feinesAcabades == true) || classe == true);
         System.out.println("Pots sortir? "+potsSortir);
 
         // Manera amb IFs
-        // if (plou == false && feines == true && classe == false) { 
+        // if (plou == false && feines == true || classe == true) { 
         //     System.out.println("Pots sortir de casa");
         // } else {
         //     System.out.println("No pots sortir de casa");
