92d318f7f513c5b234a3553327b4f41dc9ebe4b3
Marc Mas <marcmashernandez@gmail.com>
2025-10-03 17:32:21 +0200

    Act13 feta amb constant

--- CODE INSITUT CFGS/Programacio/Java Acabat/UD1/Ud1Act13MarcMas/src/main/java/org/yourcompany/yourproject/Ud1Act13MarcMas.java
+++ CODE INSITUT CFGS/Programacio/Java Acabat/UD1/Ud1Act13MarcMas/src/main/java/org/yourcompany/yourproject/Ud1Act13MarcMas.java
@@ -11,6 +11,8 @@ import java.util.Scanner;
  */
 public class Ud1Act13MarcMas {
 
+
+    public static final Double IVA = 0.21;
     public static void main(String[] args) {
         // Activitat 13
         Scanner s = new Scanner(System.in);
@@ -24,7 +26,7 @@ public class Ud1Act13MarcMas {
                 System.out.println("Calculadora de preu amb IVA");
                 System.out.println("Indica un preu"); // deman el nombre per consola
                 Float preu = s.nextFloat();
-                Float ivaPreu = (float) (preu * 0.21); // multiplic per 0.21 per saber només l'IVA
+                Float ivaPreu = (float) (preu * IVA); // multiplic per 0.21 per saber només l'IVA
                 Float sumaTotal = preu + ivaPreu; // conseguesc la suma total suamnt l'iVA al producte
                 System.out.println("L'IVA del teu producte es " + ivaPreu + " i el total seria de " + sumaTotal); // ho imprimesc
                 break;
@@ -32,7 +34,7 @@ public class Ud1Act13MarcMas {
                 System.out.println("Calculadora de IVA d'un preu total");
                 System.out.println("Indica el preu total"); // deman el nombre per consola
                 Float preuTotal = s.nextFloat();
-                Float iva = (float) (preuTotal - (preuTotal / 1.21)); // multiplic per 0.21 per saber només l'IVA
+                Float iva = (float) (preuTotal - (preuTotal / (IVA+1))); // multiplic per 0.21 per saber només l'IVA
                 System.out.println("L'IVA del teu producte es " + iva); // ho imprimesc
                 break;
             default:
