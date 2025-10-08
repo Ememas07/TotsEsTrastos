9192661918a37364154f1df62771fecaa8ee3c87
Marc Mas <marcmashernandez@gmail.com>
2025-10-08 16:47:04 +0200

    Act3 revisada

--- CODE INSITUT CFGS/Programacio/Java/UD3/Ud3Act3MarcMas/src/main/java/org/yourcompany/yourproject/Ud3Act3MarcMas.java
+++ CODE INSITUT CFGS/Programacio/Java/UD3/Ud3Act3MarcMas/src/main/java/org/yourcompany/yourproject/Ud3Act3MarcMas.java
@@ -12,8 +12,9 @@ import java.util.Scanner;
 public class Ud3Act3MarcMas {
 
     public static void main(String[] args) {
-        System.out.println("Joc d'aproximació");
-        System.out.println("L'ordinador generarà un nombre enter entre 0 i 100, i l'has d'endivinar.");
+        System.out.println("Joc d'aproximacio");
+        System.out.println("L'ordinador generara un nombre enter entre 0 i 100, i l'has d'endivinar.");
+        System.out.println("Introdueix el nombre -1 per rendir-te");
         Scanner s = new Scanner(System.in);
         System.out.println("Introdueixi un nombre"); //deman el primer nombre per consola i l'agaf com a byte
         Byte n = s.nextByte();
@@ -23,19 +24,24 @@ public class Ud3Act3MarcMas {
         if (solucio == 0) {
             solucio = 1; //per evitar que el random doni 0
         }
-        while ((int) n != solucio) { //si n no es igual que la solucio, entram
+        while ((int) n != solucio && (int) n != -1) { //si n no es igual que la solucio, entram
             if (n > solucio) {
                 System.out.println("El teu nombre es mes alt que la solucio");
-                punts += (n-solucio);
+                punts += (n - solucio);
             } else {
                 System.out.println("El teu nombre es mes baix que la solucio");
-                punts += (solucio-n); //sumam la diferencia entre punts i el nombre introduit (o viceversa)
+                punts += (solucio - n); //sumam la diferencia entre punts i el nombre introduit (o viceversa)
             }
             rondes++; //sumam una ronda
             System.out.println("Introdueixi un altre"); //deman el primer nombre per consola i l'agaf com a byte
             n = s.nextByte();
         }
-        //un pic encertam el nombre, surtim
-        System.out.println("Enhorabona! La teva puntuacio es de "+rondes+" Rondes - "+punts+" Punts");
+        //un pic encertam el nombre o ens rendim, surtim
+        if ((int) n != -1) {
+            System.out.println("Enhorabona! La teva puntuacio es de " + rondes + " Rondes - " + punts + " Punts");
+        } else {
+            System.out.println("T'has rendit :(");
+            System.out.println("La teva puntuacio es de " + rondes + " Rondes - " + punts + " Punts");
+        }
     }
 }
