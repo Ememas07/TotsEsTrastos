77056b3964dabde0c4481af1316af600e73fe367
Marc Mas <marcmashernandez@gmail.com>
2025-10-13 15:11:56 +0200

    Acts extra UD2 reformatadas ja que la contrassenya no existia

--- CODE INSITUT CFGS/Programacio/Java/UD2/UD2ActExtra2MarcMas/src/main/java/org/yourcompany/yourproject/UD2ActExtra2MarcMas.java
+++ CODE INSITUT CFGS/Programacio/Java/UD2/UD2ActExtra2MarcMas/src/main/java/org/yourcompany/yourproject/UD2ActExtra2MarcMas.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
+
 package org.yourcompany.yourproject;
 
 import java.util.Scanner;
@@ -13,13 +14,16 @@ public class UD2ActExtra2MarcMas {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Introdueix la contrassenya");
-        String input = s.next();
-        String contrassenya = "holabondia";
-        if (input.equals(contrassenya)) {
-            System.out.println("Contrassenya correcta");
+        System.out.println("Introdueix la nota de l'alumne");
+        Double nota = s.nextDouble();
+        if (nota == 10) {
+            System.out.println("Matricula d'honor");
+        } else if (nota < 10 && nota >= 9) {
+            System.out.println("Notable");
+        } else if (nota < 9 && nota >= 5) {
+            System.out.println("Aprovat");
         } else {
-            System.out.println("Contrassenya incorrecta");
+            System.out.println("Suspes");
         }
     }
 }
--- CODE INSITUT CFGS/Programacio/Java/UD2/UD2ActExtra3MarcMas/src/main/java/org/yourcompany/yourproject/UD2ActExtra3MarcMas.java
+++ CODE INSITUT CFGS/Programacio/Java/UD2/UD2ActExtra3MarcMas/src/main/java/org/yourcompany/yourproject/UD2ActExtra3MarcMas.java
@@ -14,16 +14,21 @@ public class UD2ActExtra3MarcMas {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Introdueix la nota de l'alumne");
-        Double nota = s.nextDouble();
-        if (nota == 10) {
-            System.out.println("Matricula d'honor");
-        } else if (nota < 10 && nota >= 9) {
-            System.out.println("Notable");
-        } else if (nota < 9 && nota >= 5) {
-            System.out.println("Aprovat");
-        } else {
-            System.out.println("Suspes");
+        System.out.println("Introdueix El costat 1");
+        Double c1 = s.nextDouble();
+        System.out.println("Introdueix El costat 2");
+        Double c2 = s.nextDouble();
+        System.out.println("Introdueix El costat 3");
+        Double c3 = s.nextDouble();
+        if ((c1 + c2) > c3 || (c1 + c3) > c2 || (c2 + c3) > c1 ) {
+            System.out.println("Es pot formar un triangle");
+        }
+        if (c1 == c2 && c2 == c3) {
+            System.out.println("Equilater");
+        } else if (c1 == c2 || c1 == c3 || c2 == c3) {
+            System.out.println("Isosceles");
+        } else{
+            System.out.println("Escale");
         }
     }
 }
