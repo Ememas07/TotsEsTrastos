a4687ff6cefddf897b87a4c06e8b0d5ba583b76b
Marc Mas <marcmashernandez@gmail.com>
2025-11-12 14:16:19 +0100

    programa mes net amb molt de temps

--- CODE INSTITUT CFGS/Programacio/Java/UD5/BogoSort/src/main/java/org/yourcompany/yourproject/BogoSort.java
+++ CODE INSTITUT CFGS/Programacio/Java/UD5/BogoSort/src/main/java/org/yourcompany/yourproject/BogoSort.java
@@ -13,28 +13,30 @@ public class BogoSort {
         int iteracions = 0;
         int taulaAleatoria[] = new int[taula.length];
         int numeroAleatori = 0;
-        while (!checkSort(taulaAleatoria)) {
-            buidarTaula(taulaAleatoria);
-            for (int i = 0; i < taula.length;) {
-                numeroAleatori = (int) (Math.random() * (taula.length));
-                if (taulaAleatoria[numeroAleatori] == 0) {
-                    taulaAleatoria[numeroAleatori] = taula[i];
-                    i++;
-                    iteracions++;
+        while (!checkSort(taulaAleatoria)) { //si la taula no esta ordenada 
+            buidarTaula(taulaAleatoria); //buidar
+            for (int i = 0; i < taula.length;) { //per cada registres de la taula
+                numeroAleatori = (int) (Math.random() * (taula.length)); //generam nombre aleatori
+                if (taulaAleatoria[numeroAleatori] == 0) { //si el nombres es 0 (es a dir, encara no hem omplit el registre)
+                    taulaAleatoria[numeroAleatori] = taula[i]; //assignam el registre 0, despres el registre, 1 de la taula original al valor aleatori
+                    i++; //sumam 1 al bucle
+                    iteracions++; //sumam iteracions
                 }
-                System.out.print("Iteracio " + iteracions + "     ");
+                // System.out.print("Iteracio " + iteracions + "     "); //print de iteracions perque es gracios
             }
         }
-        for (int i = 0; i < taulaAleatoria.length; i++) {
-            System.out.println(taulaAleatoria[i]);
+        System.out.println("");
+        System.out.println("Total de iteracions:" + iteracions);
+        for (int i = 0; i < taula.length; i++) {
+            taula[i] = taulaAleatoria[i]; //copiam el array a l'altre
         }
     }
 
     public static boolean checkSort(int[] taula) {
-        for (int i = 0; i < taula.length - 1; i++) {
-            for (int j = 0; j < taula.length - i - 1; j++) {
-                if (taula[j] > taula[j + 1] || taula[j] == taula[j + 1]) {
-                    return false;
+        for (int i = 0; i < taula.length - 1; i++) { //per cada element de la taula
+            for (int j = 0; j < taula.length - i - 1; j++) { //per cada element de la taula a partir del que tenim abans
+                if (taula[j] > taula[j + 1] || taula[j] == taula[j + 1]) { //si l'index i l'index seguent son iguals o el primer es major 
+                    return false; //no esta ordenat
                 }
             }
         }
@@ -43,15 +45,26 @@ public class BogoSort {
 
     public static void buidarTaula(int[] taula) {
         for (int i = 0; i < taula.length; i++) {
-            taula[i] = 0;
+            taula[i] = 0; //recorrem tot l'arrayy i l'omplim amb 0
+        }
+    }
+
+    public static void omplirTaula(int[] taula, int minim, int maxim) {
+        for (int i = 0; i < taula.length; i++) { //recorrem tot l'array
+            taula[i] = (int) (minim + Math.random() * maxim); //omplim amb nombres de 1 a 100
+        }
+    }
+
+    public static void imprimirTaula(int[] taula) {
+        for (int i = 0; i < taula.length; i++) {
+            System.out.println(taula[i]);
         }
     }
 
     public static void main(String[] args) {
         int taulaAleatoris[] = new int[5]; //cream array
-        for (int i = 0; i < taulaAleatoris.length; i++) { //recorrem tot l'array
-            taulaAleatoris[i] = (int) (1 + Math.random() * 100); //omplim amb nombres de 1 a 100
-        }
+        omplirTaula(taulaAleatoris, 1, 100);
         bogoSort(taulaAleatoris);
+        imprimirTaula(taulaAleatoris);
     }
 }
