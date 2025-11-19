04f9c168618d27c2f199af096a791c217646d349
Marc Mas <marcmashernandez@gmail.com>
2025-11-19 17:48:35 +0100

    ActTut corregida, Trimestre Creat, Act6 i Act7 fetes

--- CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act6MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act6MarcMas.java
+++ CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act6MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act6MarcMas.java
@@ -3,8 +3,10 @@
  */
 package org.yourcompany.yourproject;
 
+import java.util.Scanner;
+
 /**
- * Escriu la funció: int primitiva(int[] aposta, int[] combGuanyadora).
+ * Escriu la funció: byte primitiva(byte[] aposta, byte[] combGuanyadora).
  *
  * La funció ha de tornar el nombre d’encerts (posicions iguals) entre les dues
  * taules
@@ -13,9 +15,9 @@ package org.yourcompany.yourproject;
  */
 public class Ud5Act6MarcMas {
 
-    public static int primitiva(int[] aposta, int[] combGuanyadora) {
-        int puntuacio = 0;
-        for (int i = 0; i < aposta.length; i++) {
+    public static byte primitiva(byte[] aposta, byte[] combGuanyadora) { 
+        byte puntuacio = 0;
+        for (byte i = 0; i < aposta.length; i++) {
             if (aposta[i] == combGuanyadora[i]) {
                 puntuacio++;
             }
@@ -24,16 +26,34 @@ public class Ud5Act6MarcMas {
         return puntuacio;
     }
 
-    public static int[] generarPrimitiva() {
-        int taula[] = new int[6];
-        for (int i = 0; i < taula.length; i++) {
-            
+    public static byte[] generarPrimitiva() {
+        byte taula[] = new byte[6]; //cream un array de 6 posicions
+        for (byte i = 0; i < taula.length; i++) { //recorrem tot l'array
+            taula[i] = (byte) (1 + Math.random() * 49); //anam omplbyte amb nombres desde 1 fins a 49
         }
+        return taula;
+    }
 
+    public static byte[] rellenarPrimitiva() {
+        Scanner s = new Scanner(System.in); 
+        byte taula[] = new byte[6]; //cream un array de 6 posicions
+        for (byte i = 0; i < taula.length; i++) { //recorrem tot l'array
+            System.out.println("Introdueixi el numero " + (i + 1)); //anam demanant valors a l'usuari
+            byte numero = s.nextByte(); //assignam
+            while (numero > 49 || numero < 0) { //si l'usuari introdueix un valor invalid, el tornarem demanar
+                System.out.println("Introdueix un numero valid!"); 
+                numero = s.nextByte();
+            }
+            taula[i] = numero; //ficam el valor a l'array
+        }
         return taula;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Benvingut al meu programa de la primitiva!");
+        byte aposta[] = rellenarPrimitiva(); //omplim la nostra aposta 
+        byte combGuanyadora[] = generarPrimitiva(); //generam una combinació guanyadora aleatoria 
+        byte puntuacio = primitiva(aposta, combGuanyadora); //assignam la puntuacio amb la variable que ho compreva
+        System.out.println("La teva puntuacio es " + puntuacio); //imprim a l'usuari
     }
 }
--- CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act7MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act7MarcMas.java
+++ CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act7MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act7MarcMas.java
@@ -1,16 +1,53 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.yourcompany.yourproject;
 
+import java.util.Scanner;
+
 /**
+ *
+ * Escriu la funció: int[] senseRepetits(int[] t)
+ *
+ * que construeix i retorna una taula de la longitud que toqui ,
+ *
+ * amb els elements no repetits de la taula t
  *
  * @author Marc Mas
  */
 public class Ud5Act7MarcMas {
 
+    public static int[] senseRepetits(int[] t) {
+        int taulaNeta[] = new int[t.length];
+        for (int i = 0; i < t.length; i++) {
+            if (!nombreDuplicat(t, t[i], i)) {
+                taulaNeta[i] = t[i];
+            }
+        }
+        return taulaNeta;
+    }
+
+    public static boolean nombreDuplicat(int[] taula, int n, int indexOriginal) { //indexCerca es el principi de la cerca, ja que volem saber si el nombre hi és dues vegades o més
+        boolean duplicat = false; //assignam false per començar
+        for (int i = 0; i < taula.length; i++) {  //recorrem tota la taula
+            if (taula[i] == n && i != indexOriginal) { //si trobam l'element a dins la taula i NO està a la posició on esteim comprovant, marcam com a duplicat
+                duplicat = true;
+            }
+        }
+        return duplicat;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Programa que mostra els nombres no repetits d'un array");
+        int arrayNombres[] = new int[5];
+        for (int i = 0; i < arrayNombres.length; i++) { //recorrem l'array i l'omplim amb ints
+            System.out.println("Introdueixi un nombre enter");
+            arrayNombres[i] = s.nextInt();
+        }
+        arrayNombres = senseRepetits(arrayNombres);
+        for (int i = 0; i < arrayNombres.length; i++) {
+            System.out.print(arrayNombres[i] + ", ");
+        }
     }
 }
