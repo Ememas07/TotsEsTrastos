47f851d8a9e98a2e9e1a2f6191b8f4cc8f4a58a1
Marc Mas <marcmashernandez@gmail.com>
2025-11-24 16:03:08 +0100

    act12 començada, no entender, fare sa practica

--- CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act11MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act11MarcMas.java
+++ CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act11MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act11MarcMas.java
@@ -25,45 +25,66 @@ import java.util.Scanner;
 public class Ud5Act11MarcMas {
 
     public static boolean comprovacio(int[] correcta, int[] usuari) {
-        int encerts = 0;
-        for (int i = 0; i < correcta.length; i++) {
+        int encerts = 0; //variable que emprarem per retornar la puntuacio
+        for (int i = 0; i < correcta.length; i++) { //recorrem l'array de "correcta"
             if (correcta[i] > usuari[i]) {
-                System.out.print(">");
+                System.out.print(">"); //si el nombre que comprovam es major que el de l'usuari, imprimirem un signe de >
             } else if (correcta[i] < usuari[i]) {
-                System.out.print("<");
+                System.out.print("<"); //si no, i el nombre es menor, imprirem el signe de <
             } else {
-                encerts++;
-                System.out.print("=");
-                System.out.print("(" + usuari[i] + ")"); // mostram a l'usuari quin nombre ha posat si es correcte perque el pugui repetir
+                encerts++; //si no, el nombre ha de ser igual, afegirem un encert
+                System.out.print("="); //imprimirem un signe d'igual
             }
-            System.out.print("   ||   ");
+            System.out.print("   ||   "); //cada vegada, imprimirem un separador també
         }
-        System.out.println("");
+        System.out.println(""); //bot de linea
+        for (int i = 0; i < correcta.length; i++) {
+            System.out.print("(" + usuari[i] + ")"); // mostram a l'usuari quin nombre ha posat si es correcte perque el pugui repetir
+            System.out.print(" || "); //cada vegada, imprimirem un separador també
+        }
+        System.out.println(""); //bot de linea
         return encerts == correcta.length; //equivalent a fer un "if encerts == correcta.length return true i return false si no"
     }
 
+    // versio sense millores
+    // public static boolean comprovacio(int[] correcta, int[] usuari) {
+    //     int encerts = 0; //variable que emprarem per retornar la puntuacio
+    //     for (int i = 0; i < correcta.length; i++) { //recorrem l'array de "correcta"
+    //         if (correcta[i] > usuari[i]) {
+    //             System.out.print(">"); //si el nombre que comprovam es major que el de l'usuari, imprimirem un signe de >
+    //         } else if (correcta[i] < usuari[i]) {
+    //             System.out.print("<"); //si no, i el nombre es menor, imprirem el signe de <
+    //         } else {
+    //             encerts++; //si no, el nombre ha de ser igual, afegirem un encert
+    //             System.out.print("="); //imprimirem un signe d'igual
+    //         }
+    //         System.out.print("   ||   "); //cada vegada, imprimirem un separador també
+    //     }
+    //     System.out.println(""); //bot de linea
+    //     return encerts == correcta.length; //equivalent a fer un "if encerts == correcta.length return true i return false si no"
+    // }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Joc de La Cambra Secreta");
         System.out.println("Hauras d'introduir una serie de nombres i intentar endivinar la combinacio correcta");
         System.out.println("Quin vol que sigui el nombre maxim del nombre?"); //demanam quin rang de nombres vol l'usuari
         int maxim = s.nextInt();
-        System.out.println("Quina sera la longitut de la combinacio?");
+        System.out.println("Quina sera la longitut de la combinacio?"); // demanam longitut 
         int longitut = s.nextInt();
-        int combinacioCorrecta[] = new int[longitut];
+        int combinacioCorrecta[] = new int[longitut]; //cream un array de combinacioCorrecta i un de combinacioUsuari
         int combinacioUsuari[] = new int[longitut];
-        for (int i = 0; i < combinacioCorrecta.length; i++) {
+        for (int i = 0; i < combinacioCorrecta.length; i++) { //omplirem l'array de combinacioCorrecta amb nombres aleatoris de 1 a maxim
             combinacioCorrecta[i] = (int) (1 + Math.random() * maxim);
         }
         do {
-            demanarUsuari(combinacioUsuari);
+            demanarUsuari(combinacioUsuari); //demanarem la combinacio a l'usuari tantes vegades com faci falta fins que endivini la combinacio
         } while (!comprovacio(combinacioCorrecta, combinacioUsuari));
-        System.out.println("Has guanyat!");
+        System.out.println("Has guanyat!"); //imprim missatge de victoria
     }
 
     public static void demanarUsuari(int t[]) {
         Scanner s = new Scanner(System.in);
-        for (int i = 0; i < t.length; i++) {
+        for (int i = 0; i < t.length; i++) { //demanam a l'usuari nombres fins a omplir l'array
             System.out.println("Introdueixi el nombre " + (i + 1) + " de la seva combinacio");
             t[i] = s.nextInt();
         }
--- CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act12MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act12MarcMas.java
+++ CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act12MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act12MarcMas.java
@@ -1,16 +1,56 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.yourcompany.yourproject;
 
 /**
+ * En un joc de rol el mapa es pot implementar com una matriu on les files i les
+ * columnes representen llocs (lloc 0, lloc 1, lloc 2, etc.) que estaran
+ * connectats.
+ *
+ * Si des del lloc X podem anar cap al lloc Y, llavors la matriu a la posició
+ * [x][y] valdrà cert; en cas contrari, valdrà fals.
+ *
+ * Escriu una funció que, atesa una matriu que representa el mapa i dos llocs,
+ * indiqui si és possible viatjar des del primer lloc al segon (directament o
+ * passant per llocs intermedis).
  *
  * @author Marc Mas
  */
 public class Ud5Act12MarcMas {
 
+    public static int[][] generarMapa() {
+        int mapa[][] = new int[10][10];
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa.length; j++) {
+                if (i == 0 || i == mapa.length - 1 || j == 0 || j == mapa.length) {
+                    mapa[i][j] = 1;
+                } else {
+                    if (Math.random() >= 0.5) {
+                        mapa[i][j] = 1;
+                    } else {
+                        mapa[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        return mapa;
+    }
+
+    public static void imprimirArray2D(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[i][j] + ", ");
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Joc de rol amb mapa i coordenades");
+        int mapa[][] = generarMapa();
+        imprimirArray2D(mapa);
+        
     }
 }
