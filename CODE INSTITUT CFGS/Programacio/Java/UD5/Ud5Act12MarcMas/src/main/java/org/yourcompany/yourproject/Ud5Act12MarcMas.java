b1d3a22780a09d348851d176e91ece8377884415
Marc Mas <marcmashernandez@gmail.com>
2025-11-27 18:25:24 +0100

    act12 feta amb extres

--- CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act12MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act12MarcMas.java
+++ CODE INSTITUT CFGS/Programacio/Java/UD5/Ud5Act12MarcMas/src/main/java/org/yourcompany/yourproject/Ud5Act12MarcMas.java
@@ -3,6 +3,8 @@
  */
 package org.yourcompany.yourproject;
 
+import java.util.Scanner;
+
 /**
  * En un joc de rol el mapa es pot implementar com una matriu on les files i les
  * columnes representen llocs (lloc 0, lloc 1, lloc 2, etc.) que estaran
@@ -19,38 +21,68 @@ package org.yourcompany.yourproject;
  */
 public class Ud5Act12MarcMas {
 
-    public static int[][] generarMapa() {
-        int mapa[][] = new int[10][10];
-        for (int i = 0; i < mapa.length; i++) {
+    public static boolean comprovarTrajecte(boolean[][] mapa, int lloc1, int lloc2) {
+        return mapa[lloc1][lloc2];
+
+        // aquest return es equivalent a:
+        // if (mapa[lloc1][lloc2] == 1) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+    }
+
+    public static boolean[][] generarMapa() {
+        boolean mapa[][] = new boolean[10][10]; //cream un array de 10x10 de booleans
+        for (boolean[] mapa1 : mapa) {
+            //recorrem totes les files de l'array
             for (int j = 0; j < mapa.length; j++) {
-                if (i == 0 || i == mapa.length - 1 || j == 0 || j == mapa.length) {
-                    mapa[i][j] = 1;
-                } else {
-                    if (Math.random() >= 0.5) {
-                        mapa[i][j] = 1;
-                    } else {
-                        mapa[i][j] = 0;
-                    }
-                }
+                //recorrem totes les columnes
+                mapa1[j] = Math.random() >= 0.5; // generam un math.random, si el nombre es mes gran de 0.5, posam true, si no, posam false
+                // equivalent a:
+                // if (Math.random() >= 0.5) {
+                //     mapa[i][j] = true;
+                // } else {
+                //     mapa[i][j] = false;
+                // }
+                // }
             }
         }
-
         return mapa;
     }
 
-    public static void imprimirArray2D(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                System.out.print(array[i][j] + ", ");
+    public static void imprimirArray2D(boolean[][] array, char ocupats, char lliures) {
+        for (boolean[] array1 : array) { //recorrem totes les columnes
+            for (int j = 0; j < array.length; j++) { //recorrem totes les files 
+                if (array1[j] == true) { //si la posicio es true, pintam el caracter de ocupat
+                    System.out.print(ocupats);
+                } else { //si no, pinta el caracter de lliure
+                    System.out.print(lliures);
+                }
+                System.out.print(" | "); //pintam un separador entre elements de la mateixa fila
             }
-            System.out.println("");
+            System.out.println(""); //bot de linea entre columnes
         }
     }
 
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         System.out.println("Joc de rol amb mapa i coordenades");
-        int mapa[][] = generarMapa();
-        imprimirArray2D(mapa);
-        
+        boolean mapa[][] = generarMapa(); //el nostre array serà generat per aquesta funcio
+        System.out.println("Quin caracter vols per les posicions buides?");
+        char buides = s.next().charAt(0); //agafam el caracter per console
+        System.out.println("Quin caracter vols per les altres?");
+        char ocupats = s.next().charAt(0);
+        imprimirArray2D(mapa, ocupats, buides); //imprimim l'array (com a opcional)
+        System.out.println("Introdueix primer lloc"); //introduim els dos llocs que volem mirar
+        int lloc1 = s.nextInt();
+        System.out.println("Introdueix segon lloc");
+        int lloc2 = s.nextInt();
+        if (comprovarTrajecte(mapa, lloc1, lloc2)) { //miram si hi podem anar, si hi podem anar imprimim "si que" i si no imprimim "no"
+            System.out.print("Si que");
+        } else {
+            System.out.print("No");
+        }
+        System.out.println(" es pot anar"); //part final del print
     }
 }
