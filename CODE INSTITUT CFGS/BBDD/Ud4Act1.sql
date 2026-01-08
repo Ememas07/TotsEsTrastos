e5872038f9adf15b8456caf1ad619ffe18d8c4e1
Marc Mas <marcmashernandez@gmail.com>
2026-01-08 16:27:36 +0100

    act1

---
+++ CODE INSTITUT CFGS/BBDD/Ud4Act1.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Alumne(  
+    dni int(8) NOT NULL PRIMARY KEY,
+    Nom VARCHAR(30) NOT NULL,
+    Ll1 VARCHAR(30) NOT NULL,
+    Ll2 VARCHAR(30)
+);
+
+CREATE TABLE Assignatura(  
+    CodAssig VARCHAR(10) NOT NULL PRIMARY KEY,
+    Nom VARCHAR(30) NOT NULL,
+    NomHores Int(4) NOT NULL
+);
+
+CREATE TABLE Inscripció(
+    dni int(8),
+    CodAssig VARCHAR(10),
+    Qualificació INT(2),
+    constraint fk_dniAlumne FOREIGN KEY(dni) references Alumne(dni),
+    constraint fk_codAssig FOREIGN KEY(CodAssig) references Assignatura(CodAssig)
+)
