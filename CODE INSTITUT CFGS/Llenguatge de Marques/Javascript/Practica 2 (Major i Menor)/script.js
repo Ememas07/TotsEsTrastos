e086eed92c7bcc9712d65daa40db3dff0cfde464
Marc Mas <marcmashernandez@gmail.com>
2026-01-14 17:34:29 +0100

    Practica 3 JS

---
+++ CODE INSTITUT CFGS/Llenguatge de Marques/Javascript/Practica 2 (Major i Menor)/script.js
@@ -0,0 +1,9 @@
+function majorMenor() {
+    var n1 = parseInt(document.getElementById("n1").value);
+    var n2 = parseInt(document.getElementById("n2").value);
+    if (n1 > n2) {
+        console.log("El nombre més gran es N1")
+    } else {
+        console.log("El nombre més gran es N2")
+    }
+}
--- CODE INSTITUT CFGS/Llenguatge de Marques/Javascript/Practica 3 (Nombres Primers)/script.js
+++ CODE INSTITUT CFGS/Llenguatge de Marques/Javascript/Practica 3 (Nombres Primers)/script.js
@@ -1,15 +1,21 @@
+function nombrePrimer() {
+    var n = parseInt(document.getElementById("n1").value);
+    if (n <= 1) {
+        return false;
+    }
+    if (n == 2) {
+        return true;
+    }
+    for (let i = 2, s = Math.sqrt(n); i <= s; i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
 
-
-
-// if (num <= 1)
-//             return false; // 1 no és primer
-//         if (num == 2)
-//             return true; // 2 és primer
-//         if (num % 2 == 0)
-//             return false; // tots es numeros divisibles entre 2 no són primers
-
-//         // Comprovació de si és primer o no
-//         for (int j = 3; j <= Math.sqrt(num); j += 2) {
-//             if (num % j == 0)
-//                 return false;
-//         }
+function comprovar() {
+    if (nombrePrimer()) {
+        console.log("El nombre es primer")
+    } else {
+        console.log("El nombre no es primer")
+    }
+}
