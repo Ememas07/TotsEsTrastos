a41c996d435e22913ff0a8c221b96e59e0d8ff97
Marc Mas <marcmashernandez@gmail.com>
2026-03-18 16:37:51 +0100

    Començ

---
+++ CODE INSTITUT CFGS/Programacio/Java/UD8/Enum2WithAVengeanceMarcMas/src/main/java/org/Player/Player.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package org.Player;
+
+/**
+ *
+ * @author Marc Mas
+ */
+public class Player {
+
+}
