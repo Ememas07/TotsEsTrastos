77a7ee5eebc89e16a29443772fb0357c0d344fd6
Marc Mas <marcmashernandez@gmail.com>
2026-03-27 11:25:37 +0100

    Inserts

---
+++ CODE INSTITUT CFGS/BBDD/UD5/SimulacreInserts.sql
@@ -0,0 +1,31 @@
+-- Active: 1774606104932@@192.168.1.140@33700@centreFormacio
+INSERT poble (nom) VALUES ('Maria de la salut');
+INSERT poble (nom) VALUES ('Sineu');
+INSERT poble (nom) VALUES ('Lloret');
+INSERT poble (nom) VALUES ('Lloseta');
+INSERT poble (nom) VALUES ('Lluc');
+INSERT poble (nom) VALUES ('Palma');
+INSERT poble (nom) VALUES ('Inca');
+INSERT poble (nom) VALUES ('Manacor');
+INSERT poble (nom) VALUES ('Ariany');
+INSERT poble (nom) VALUES ('Petra');
+INSERT poble (nom) VALUES ('Esporles');
+INSERT poble (nom) VALUES ('Selva');
+
+
+INSERT alumnes (dni, nom, llinatge1, dataNaixement, poble) VALUES (14587289,"Marc","Mas",'2007-02-15',1);
+INSERT alumnes (dni, nom, llinatge1, dataNaixement, poble) VALUES (14587458,"Pere","Munar",'2007-05-23',1);
+INSERT alumnes (dni, nom, llinatge1, dataNaixement, poble) VALUES (04051378,"Ashley","Dreijer",'2007-09-09',7);
+INSERT alumnes (dni, nom, llinatge1, dataNaixement, poble) VALUES (04691297,"Rokiow","Bum",'2011-03-16',3);
+INSERT alumnes (dni, nom, llinatge1, dataNaixement, poble) VALUES (78102913,"Juan","Antonio",'1979-11-13-',6);
+INSERT alumnes (dni, nom, llinatge1, dataNaixement, poble) VALUES (14895278,"Mateu","Ripoll",'2003-03-03',8);
+INSERT alumnes (dni, nom, llinatge1, dataNaixement, poble) VALUES (15863210,"Joan Pere","Gelabert",'2008-11-02',4);
+
+INSERT departament (nom) VALUES ('Matematiques');
+INSERT departament (nom) VALUES ('Fisica');
+INSERT departament (nom) VALUES ('Quimica');
+INSERT departament (nom) VALUES ('Biologia');
+INSERT departament (nom) VALUES ('Informatica');
+INSERT departament (nom) VALUES ('Tecnologia');
+INSERT departament (nom) VALUES ('Angles');
+INSERT departament (nom) VALUES ('Alemany');
