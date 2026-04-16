314bc6a0e57756c3dbbbe7f23ffaf0f043e58422
Marc Mas <marcmashernandez@gmail.com>
2026-04-16 09:16:48 +0200

    Ud6Act1 de BBDD i ud13act1 progr

--- CODE INSTITUT CFGS/BBDD/UD6/Ud6Act1MarcMas.sql
+++ CODE INSTITUT CFGS/BBDD/UD6/Ud6Act1MarcMas.sql
@@ -1,3 +1,4 @@
+-- Active: 1744015454928@@127.0.0.1@33700@Test
 -- Calcular area cercle: PI * r^2
 -- Calcular volum cilindre PI * r^2 * altura
 
@@ -7,14 +8,28 @@ BEGIN
     SET @area = PI() * radi * radi;
 END
 
+CREATE PROCEDURE areaCercle2(IN radi INT, OUT area FLOAT)
+BEGIN
+    set area = PI() * radi * radi;
+END
+
 CREATE PROCEDURE volumCilindre(IN radi INT,IN altura INT)
 BEGIN
     DECLARE volum float;
     CALL areaCercle(radi);
     SET @volum =  @area * altura;
 END
-call areaCercle(7);
-select @area;
 
-call volumCilindre(5,10);
+CREATE PROCEDURE volumCilindre2(IN radi INT,IN altura INT, OUT volum FLOAT)
+BEGIN
+    CALL areaCercle2(radi,@area);
+    SET volum =  @area * altura;
+END
+
+call areaCercle2 (7, @test);
+
+select @test;
+
+call volumCilindre2 (5, 10, @test);
+
 select @volum;
--- CODE INSTITUT CFGS/Programacio/Java/UD13/Ud13Act1MarcMas/src/ud13act1marcmas/Ud13Act1MarcMas.java
+++ CODE INSTITUT CFGS/Programacio/Java/UD13/Ud13Act1MarcMas/src/ud13act1marcmas/Ud13Act1MarcMas.java
@@ -6,6 +6,7 @@ package ud13act1marcmas;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -28,18 +29,28 @@ public class Ud13Act1MarcMas {
         switch (opcio) {
             case 1:
                 Activitat1();
+                break;
             case 2:
                 Activitat2();
+                break;
             case 3:
                 Activitat3();
+                break;
             case 4:
                 Activitat4();
+                break;
             case 5:
                 Activitat5();
+                break;
             case 6:
                 Activitat6();
+                break;
             case 7:
                 Activitat7();
+                break;
+            case 8:
+                Activitat8();
+                break;
         }
     }
 
@@ -187,26 +198,53 @@ public class Ud13Act1MarcMas {
         try {
             Scanner s = new Scanner(System.in);
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:33700/f1_2023", "root", "holaquetal");
-            String sql = "SELECT * FROM Races";
+            String sql = "SELECT * FROM drivers";
             Statement st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             ResultSet gp = st.executeQuery(sql);
-            gp.afterLast();
-            while (gp.previous()) {
-                System.out.println("Carreres: " + gp.getInt("race_id") + " - " + gp.getString("race_name"));
+            while (gp.next()) {
+                System.out.println(gp.getInt("driver_id") + ":" + gp.getString("first_name") + " " + gp.getString("last_name"));
             }
+            System.out.println("Quin pilot vols borrar? (posa només el nom, no el llinatge)");
+            String nom = s.next();
+            String sql2 = "DELETE FROM drivers WHERE first_name = \'" + nom + "\'";
+            // se podria fer SQL injection si posam un nom OR 1=1
+            System.out.println(sql2);
+            int filesAfectades = st.executeUpdate(sql2);
+            System.out.println("S'han canviat " + filesAfectades + " Files");
 
-            System.out.println("ID del gran premi?");
-            int id = s.nextInt();
-            String sql2 = "select * from results left join drivers USING (driver_id) left join teams USING (team_id) where points > 1 && race_id = " + id;
-            Statement st2 = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            ResultSet td = st2.executeQuery(sql2);
-            td.afterLast();
-            while (td.previous()) {
-                System.out.println(td.getString("first_name") + " " + td.getString("last_name") + " - " + td.getString("team_name"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getErrorCode());
+            System.out.println(e.getSQLState());
+            System.out.println("Error en la connexió");
+        }
+    }
+
+    public static void Activitat8() {
+        // 13.8 Dissenyar una aplicació que mostri un informe amb tots els pilots que han puntuat, mostrant el nom, equip, puntuació i millor posició.
+        // L'aplicació ha de sol·licitar la posició mínima que han ocupat.
+        // Utilitza una consulta amb paràmetres. 
+
+        try {
+            Scanner s = new Scanner(System.in);
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:33700/f1_2023", "root", "holaquetal");
+            String sql = "select DISTINCT (driver_id),first_name,last_name,team_name,points,position from results left join drivers USING (driver_id) left join teams using (team_id) where position < ? order by position";
+            // String sql = "select DISTINCT (driver_id),first_name,last_name,team_name,points,position from results left join drivers USING (driver_id) left join teams using (team_id) WHERE position < ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, 5);
+            System.out.println("Quina posicio minima vol?");
+            int pos = s.nextInt();
+            ps.setInt(1, pos);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(rs.getString("first_name") + " " + rs.getString("last_name") + " " + rs.getString("team_name") + " " + rs.getInt("position") + " ");
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println(e.getErrorCode());
+            System.out.println(e.getSQLState());
             System.out.println("Error en la connexió");
         }
     }
