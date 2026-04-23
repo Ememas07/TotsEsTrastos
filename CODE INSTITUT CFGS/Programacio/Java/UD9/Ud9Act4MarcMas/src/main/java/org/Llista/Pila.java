3c5551c557aa17d96cff89b9c195471117118649
Marc Mas <marcmashernandez@gmail.com>
2026-04-23 11:01:23 +0200

    Format

--- CODE INSTITUT CFGS/Programacio/Java/UD9/Ud9Act4MarcMas/src/main/java/org/Llista/Pila.java
+++ CODE INSTITUT CFGS/Programacio/Java/UD9/Ud9Act4MarcMas/src/main/java/org/Llista/Pila.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-
 package org.Llista;
 
 /**
@@ -10,8 +9,12 @@ package org.Llista;
  * @author Marc Mas
  */
 public interface Pila {
+
     void apilar(int numero);
+
     void desapilar();
+
     void desapilarTots();
+
     void mostrarElements();
 }
