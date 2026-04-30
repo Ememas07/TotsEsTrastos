1ea0c4dada84b11793bf7eafaea1ef5246d7104e
Marc Mas <marcmashernandez@gmail.com>
2026-04-30 20:06:20 +0200

    Biblioteca reordenat

---
+++ CODE INSTITUT CFGS/Entorns de Desenvolupament/XMI/Enginyeria Inversa/Espectaculo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Marc Mas
+ */
+public class Espectaculo {
+
+    private String nombre;
+    private String lugar;
+    private String momento;
+}
